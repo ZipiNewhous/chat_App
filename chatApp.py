8456cfcbc50447170f4dfdb55e3bc158f8212aa4
zipi <handesaim3a6@mbjcomp.org.il>
2023-08-23 14:29:29 +0300

    login function

--- chatApp.py
+++ chatApp.py
@@ -1,23 +1,41 @@
 from flask import Flask, request, render_template
+import csv
 
 
 app = Flask(__name__)
 
-@app.route('/', methods=['POST'])
+
+@app.route('/register', methods=['POST, GET'])
 def homePage():
-    if methods=='POST'
+    # on submit
+    if request.methods=='POST':
+        userName = request.form["username"]
+        password = request.form["password"]
+        #exist user move to the login page
+        if checkIfUserExist(userName, password):
+            return redirect(url_for('login.html'))
+        addUser(userName, password)
+        return redirect(url_for('lobby.html'))
+
+    # on loading    
+    else:
+        return render_template('register.html')
 
 
 
-
-    return render_template('register.html')
-
-
-
-@app.route('/login')
+@app.route('/login', methods=['POST', 'GET'])
 def loginPage():
-    return render_template('login.html')
+    if methods=='POST':
+        userName = request.form["username"]
+        password = request.form["password"]
+        if ! checkIfUserExist(userName, password):
+            return redirect(url_for('register'))
+        return redirect(url_for('lobby.html'))
 
+    else:
+        return render_template('login.html')
+
+        
 
 
 
